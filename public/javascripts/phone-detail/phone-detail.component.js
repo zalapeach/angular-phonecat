2a0871243a8c284abae4d7e36f6a7bdf9acc7256
Juan Carlos <zalapeach@gmail.com>
2019-06-25 16:14:12 -0500

    add event handlers

--- public/javascripts/phone-detail/phone-detail.component.js
+++ public/javascripts/phone-detail/phone-detail.component.js
@@ -5,9 +5,19 @@ angular
   controller: ['$http', '$routeParams',
     function PhoneDetailController($http, $routeParams) {
       var self = this;
+
+      self.setImage = function setImage(imageUrl) {
+        self.mainImageUrl = imageUrl;
+      };
+
+      self.onDblclick = function onDblclick(imageUrl) {
+        alert('You double clicked image: ' + imageUrl);
+      };
+
       $http.get('javascripts/phones/' + $routeParams.phoneId + '.json')
       .then(function(response){
         self.phone = response.data;
+        self.setImage(self.phone.images[0]);
       });
     }
   ]
