4b48f8c48670315952dd495f039229253eb2296d
Juan Carlos <zalapeach@gmail.com>
2019-06-24 20:49:32 -0500

    use  for http requests

--- public/javascripts/phone-list/phone-list.component.js
+++ public/javascripts/phone-list/phone-list.component.js
@@ -2,25 +2,12 @@ angular
 .module('phonecatApp')
 .component('phoneList', {
   templateUrl: '/javascripts/phone-list/phone-list.template.html',
-  controller: function PhoneListController() {
-    this.user = 'World';
-    this.phones = [
-      {
-        name: 'Nexus S.',
-        snippet: 'Fast just got faster with Nexus S.',
-        age: 1
-      },
-      {
-        name: 'Motorola XOOM with Wi-Fi',
-        snippet: 'The Next, Next generation tablet.',
-        age: 2
-      },
-      {
-        name: 'Motorola XOOM',
-        snippet: 'The Next, Next generation tablet.',
-        age: 3
-      }
-    ];
-    this.orderProp = 'age';
-  }
+  controller: ['$http', function PhoneListController($http) {
+    var self = this;
+    self.orderProp = 'age';
+    self.user = 'World'
+    $http.get('javascripts/phones.json').then(function(response) {
+      self.phones = response.data;
+    });
+  }]
 });
