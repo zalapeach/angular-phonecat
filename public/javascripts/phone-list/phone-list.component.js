0a9b4d8c2c1ed1f773cb972f09c3a93d74820f40
Juan Carlos <zalapeach@gmail.com>
2019-06-24 20:30:01 -0500

    Add a sort filter

--- public/javascripts/phone-list/phone-list.component.js
+++ public/javascripts/phone-list/phone-list.component.js
@@ -7,16 +7,20 @@ angular
     this.phones = [
       {
         name: 'Nexus S.',
-        snippet: 'Fast just got faster with Nexus S.'
+        snippet: 'Fast just got faster with Nexus S.',
+        age: 1
       },
       {
         name: 'Motorola XOOM with Wi-Fi',
-        snippet: 'The Next, Next generation tablet.'
+        snippet: 'The Next, Next generation tablet.',
+        age: 2
       },
       {
         name: 'Motorola XOOM',
-        snippet: 'The Next, Next generation tablet.'
+        snippet: 'The Next, Next generation tablet.',
+        age: 3
       }
     ];
+    this.orderProp = 'age';
   }
 });
